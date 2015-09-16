c2591e387c08f37f92f7af8f14255588a1f03f35
fengya <lifengyachina@gmail.com>
2015-09-15 18:26:39 -0700

    Add new file: StackDataModel.java This file is the data model for this application, it should be initialized when the Android application starts.

---
+++ app/src/main/java/playdoh/stackapp/StackDataModel.java
@@ -0,0 +1,52 @@
+package playdoh.stackapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by fengyali on 9/15/15.
+ * This Class is designed as the data model of this application.For short: The M for MVC;
+ * It supplies pop, push, and getStackContent method for further use in Android Classes;
+ */
+public class StackDataModel {
+
+    private ArrayList<Integer> stack_model;
+
+    private int maxIndex() {
+//        if(stack_model.isEmpty()){
+//            throw new Exception("Attempt to get the index of an empty array.");
+//        }
+        return stack_model.size() - 1;
+    }
+
+    public Integer pop(){
+        if(!stack_model.isEmpty()){
+            return stack_model.remove(this.maxIndex());
+        }else{
+            return null;
+        }
+    }
+
+    public boolean push(Integer number){
+        if(stack_model.size() >= 3){
+            return false;
+        }else if(number > 9 || number < 0) {
+            return false;
+        }else{
+            stack_model.add(stack_model.size(),number);
+            return true;
+        }
+    }
+
+    public String getStackContent(){
+        String list_content = "";
+        for(int i=0; i<stack_model.size(); i++){
+            if(i == 0){
+                list_content += stack_model.get(i).toString();
+            }else{
+                list_content += " ";
+                list_content += stack_model.get(i).toString();
+            }
+        }
+        return "[" + list_content + "]";
+    }
+}
